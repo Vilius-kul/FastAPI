953b2f1acfe64b82cf55299996ff15c2f743e04f
vilius <vilius.kulinas@gmail.com>
2022-02-03 19:58:36 +0000

    routing added, restructure

--- realistic_api/main.py
+++ realistic_api/main.py
@@ -1,13 +1,27 @@
+from unittest import removeResult
+
 import fastapi
 import uvicorn
+from starlette.staticfiles import StaticFiles
+
+from api import weather_api
+from views import home
 
 api = fastapi.FastAPI()
 
 
-@api.get("/")
-def index():
-    return "Hello weather app!"
+def configure():
+    configure_routing()
+
+
+def configure_routing():
+    api.mount("/static", StaticFiles(directory="static"), name="static")
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:api", port=8000, host="127.0.0.1")
+    configure()
+    uvicorn.run("main:api", reload=True, port=8000, host="127.0.0.1")
+else:
+    configure()
