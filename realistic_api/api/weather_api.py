a3454659427b2667d018428cf71368414d2e62fd
vilius <vilius.kulinas@gmail.com>
2022-02-06 14:08:54 +0000

    some validation added

--- realistic_api/api/weather_api.py
+++ realistic_api/api/weather_api.py
@@ -2,15 +2,24 @@ from typing import Optional
 
 import fastapi
 from models.location import Location
+from models.validation_error import ValidationError
 from services import openweather_service
 
 router = fastapi.APIRouter()
 
 
 @router.get("/api/weather/{city}")
-async def weather(loc: Location = fastapi.Depends(), units: Optional[str] = "metric"):
-    report = await openweather_service.get_report_async(
-        loc.city, loc.state, loc.country, units
-    )
+async def weather(loc: Location = fastapi.Depends(), units: str = "metric"):
 
-    return report
+    try:
+        return await openweather_service.get_report_async(
+            loc.city, loc.state, loc.country, units
+        )
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
+
+    except Exception as x:
+        print(f"Server crashed while processing request: {x}")
+        return fastapi.Response(
+            content="Error processing your request.", status_code=500
+        )
